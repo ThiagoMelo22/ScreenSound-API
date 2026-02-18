2da5cba0d0a9a81882a1c758319f63c245936c81
ThiagoMelo22 <thiagomelo329@hotmail.com>
2026-02-18 15:20:32 -0300

    Apply gitignore

--- ScreenSound-API/obj/Debug/net6.0/ScreenSound-API.AssemblyInfo.cs
+++ ScreenSound-API/obj/Debug/net6.0/ScreenSound-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa84ab9694b934251c5c0808a56dd532d9007f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
